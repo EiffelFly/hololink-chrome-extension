e9522cb63c9d2aeaf755a7fc58f315cd38997cc7
eiffelfly <eric525282@gmail.com>
2020-06-11 16:00:54 +0800

    增加功能：依據使用者登入狀況切換popup

--- popup.js
+++ popup.js
@@ -1,4 +1,24 @@
 
+// check 使用者是否有登入 — Check login cookie，如果沒有則顯示 popup_notlogin.html 導到 Hololink 的登入頁面
+
+$(function(){
+    chrome.cookies.get({"url":"https://hololink.co/", "name":"sessionid"}, function(cookie){
+        if (cookie){
+            chrome.browserAction.setPopup({"popup":"popup.html"}, function(){
+                console.log('user already logged in');
+            });   
+        }
+        else{
+            chrome.browserAction.setPopup({"popup":"popup_login.html"}, function(){
+                console.log('user not log in'); 
+            });
+            
+        } 
+    });
+});
+
+
+
 $(function(){
     chrome.tabs.query({active:true, currentWindow:true},function(tab){ /*之所以要放上 currentWindow 的原因在於如果不加這個指令，系統會以為你是指 popup，這樣會回傳 undefined */
         var title = tab[0].title;
@@ -9,12 +29,15 @@ $(function(){
     });
 });
 
+
+
+
+
 /*listen clipper 是否成功擷取資料，並且存入 */
 chrome.runtime.onMessage.addListener(function(request,sender){ 
     if (request.action == "gotText"){
         var fullText = request.source;
         console.log(fullText)
-        
     }
 });
 
@@ -29,7 +52,7 @@ function onPopupLoad(){
     });
 }
 
-window.onload = onPopupLoad; /window.onload會等網頁的全部內容，包括圖片，CSS及<iframe>等外部內容載入後才會觸發*/
+window.onload = onPopupLoad; //window.onload會等網頁的全部內容，包括圖片，CSS及<iframe>等外部內容載入後才會觸發*/
 
 
 /** 
