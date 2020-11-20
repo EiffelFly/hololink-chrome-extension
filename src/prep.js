dc7a3e920ec4355fa8dae79ea87551111e0d969b
eiffelfly <eric525282@gmail.com>
2020-11-20 10:19:11 +0800

    add bootstrap.js and modify tooltip

--- src/prep.js
+++ src/prep.js
@@ -7,33 +7,46 @@ document.body.appendChild(hololink_toolbar_container);
 var highlight_button = document.createElement('button');
 highlight_button.setAttribute('class', 'hololink-toolbar-button');
 highlight_button.setAttribute('id', 'hololink_toolbar_highlight');
+highlight_button.setAttribute('data-toggle', 'tooltip');
+highlight_button.setAttribute('data-placement', 'bottom');
+highlight_button.setAttribute('title', 'Highlight');
 var highlighte_img = document.createElement('img');
 highlight_img_path = chrome.runtime.getURL("img/highlighter.svg");
 highlighte_img.setAttribute('src', highlight_img_path);
 highlighte_img.setAttribute('width', 20);
 highlighte_img.setAttribute('height', 20);
 highlight_button.appendChild(highlighte_img);
+highlight_tooltip = document.createElement('span');
+highlight_tooltip.innerHTML = 'Highlight';
+highlight_tooltip.setAttribute('class', 'hololink-toolbar-tooltip')
 
 var annotate_button = document.createElement('button');
 annotate_button.setAttribute('class', 'hololink-toolbar-button');
 annotate_button.setAttribute('id', 'hololink_toolbar_annotate');
+annotate_button.setAttribute('data-toggle', 'tooltip');
+annotate_button.setAttribute('data-placement', 'bottom');
+annotate_button.setAttribute('title', 'Annotate');
 var annotate_img = document.createElement('img');
 highlight_img_path = chrome.runtime.getURL("img/chat.svg");
 annotate_img.setAttribute('src', highlight_img_path);
 annotate_img.setAttribute('width', 20);
 annotate_img.setAttribute('height', 20);
 annotate_button.appendChild(annotate_img);
+annotate_tooltip = document.createElement('span');
+annotate_tooltip.innerHTML = 'Annotate';
+annotate_tooltip.setAttribute('class', 'hololink-toolbar-tooltip')
 
 hololink_toolbar_container.appendChild(highlight_button);
 hololink_toolbar_container.appendChild(annotate_button);
 
 $('.hololink-toolbar-button').on("click", function(e){
-    
+
 })
 
 
 
 console.log('selection sanity check')
+
 
 // Lets listen to mouseup DOM events.
 document.addEventListener('mouseup', function (e) {
@@ -42,6 +55,7 @@ document.addEventListener('mouseup', function (e) {
     if (selection.length > 0) {
       render_tooltip(position.x, position.y, selection);
     }
+    $('[data-toggle="tooltip"]').tooltip()
     console.log(position)
 }, false);
 
