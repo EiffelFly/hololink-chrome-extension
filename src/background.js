e91bec838ad7214eb35d1e5daba795a844201dac
eiffelfly <eric525282@gmail.com>
2020-12-03 17:05:52 +0800

    change if structure of content_script_change_status message

--- src/background.js
+++ src/background.js
@@ -45,13 +45,15 @@ function got_user_data_of_current_page_handler(response){
             console.log(json)
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 // send warning to content_script that Hololink doesn't have this article
-
-                if (json.highlight[0].message && json.highlight[0].message == "Hololink doesn't have this article"){
-                    chrome.tabs.sendMessage(tabs[0].id, {action: 'content_script_change_status', message:"hololink_doesnt_have_this_article", user:json.user, csrf_token:csrf_token, session_id:session_id});
-                } else if (json.highlight.length) {
+                if (json.highlight.length){
+                    if (json.highlight[0].message && json.highlight[0].message == "Hololink doesn't have this article"){
+                        chrome.tabs.sendMessage(tabs[0].id, {action: 'content_script_change_status', message:"hololink_doesnt_have_this_article", user:json.user, csrf_token:csrf_token, session_id:session_id});
+                    } else if (json.highlight.length) {
+                        chrome.tabs.sendMessage(tabs[0].id, {action: 'content_script_change_status', message:"hololink_have_this_article", user:json.user, highlight:json.highlight, csrf_token:csrf_token, session_id:session_id});
+                    }
+                } else {
                     chrome.tabs.sendMessage(tabs[0].id, {action: 'content_script_change_status', message:"hololink_have_this_article", user:json.user, highlight:json.highlight, csrf_token:csrf_token, session_id:session_id});
-                }
-                
+                } 
             });
             
             
